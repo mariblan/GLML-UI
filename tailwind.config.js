1938a95420baa19d0d6dd7677fd3f74b11e12c31
mariblan <99274158+mariblan@users.noreply.github.com>
2024-01-28 20:01:06 +0100

    Added tailwind initial config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,39 @@
 /** @type {import('tailwindcss').Config} */
+
+import defaultTheme from "tailwindcss/defaultTheme";
+
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      xxs: "320px",
+      xs: "475px",
+      ...defaultTheme.screens,
+      '3xl': "1920px",
+    },
+    fontSize: {
+      xxs: '0.625rem',
+      ...defaultTheme.fontSize,
+    },
+    extend: {
+      scale: {
+        102: '1.02',
+      },
+      colors: {
+        skin: {
+          primary: 'var(--primary)',
+          secondary: 'var(--secondary)',
+          tertiary: 'var(--tertiary)',
+          quaternary: 'var(--quaternary)',
+          quinary: 'var(--quinary)',
+          senary: 'var(--senary)',
+          success: 'var(--success)',
+          warning: 'var(--warning)',
+          failure: 'var(--failure)',
+        },
+      },
+    },
   },
   plugins: [],
 };
