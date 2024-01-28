d504e65fdc152b81ecbf6eac5bc88a7f3dd9ac47
mariblan <99274158+mariblan@users.noreply.github.com>
2024-01-28 19:29:32 +0100

    The router was created in the App component for future redirection and the Navbar, the Footer, the Layout component and the Home component were created and imported to be used

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import './App.css'
-import Layout from 'src/pages/Layout'
+import Layout from 'src/pages/Layout';
+import Home from 'src/pages/Home';
+import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } from 'react-router-dom';
 
 function App() {
-  
+  const routes = createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+    </Route>
+  );
+  const router = createBrowserRouter(routes);
 
-  return (
-    <>
-      <Layout />
-    </>
-  )
+  return <RouterProvider router={router} />;
 }
 
 export default App
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+        <footer>
+            <p>GLML</p>
+        </footer>
+    </div>
+  )
+}
+
+export default Footer
