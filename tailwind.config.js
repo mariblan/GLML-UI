6e08b28f63a7a66b30acce844b8f800cc3922ff7
mariblan <99274158+mariblan@users.noreply.github.com>
2024-02-15 21:33:57 +0100

    tailwind configuration with personalize themes done and tested in layout and navbar styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,35 +2,39 @@
 
 import defaultTheme from "tailwindcss/defaultTheme";
 
-
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/pages/**/*.{js,jsx,ts,tsx}",
+    "./src/sections/**/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     screens: {
       xxs: "320px",
       xs: "475px",
       ...defaultTheme.screens,
-      '3xl': "1920px",
+      "3xl": "1920px",
     },
     fontSize: {
-      xxs: '0.625rem',
+      xxs: "0.625rem",
       ...defaultTheme.fontSize,
     },
     extend: {
       scale: {
-        102: '1.02',
+        102: "1.02",
       },
       colors: {
         skin: {
-          primary: 'var(--primary)',
-          secondary: 'var(--secondary)',
-          tertiary: 'var(--tertiary)',
-          quaternary: 'var(--quaternary)',
-          quinary: 'var(--quinary)',
-          senary: 'var(--senary)',
-          success: 'var(--success)',
-          warning: 'var(--warning)',
-          failure: 'var(--failure)',
+          primary: "var(--primary)",
+          secondary: "var(--secondary)",
+          tertiary: "var(--tertiary)",
+          quaternary: "var(--quaternary)",
+          quinary: "var(--quinary)",
+          senary: "var(--senary)",
+          success: "var(--success)",
+          warning: "var(--warning)",
+          failure: "var(--failure)",
         },
       },
     },
